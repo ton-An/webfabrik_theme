c57c3043b1f018bdbf1e9fb7e943de6f1d3b45be
Anton <anton@antons-webfabrik.eu>
2025-03-17 13:24:08 +0100

    feat: add more granular controls to edge fade

--- lib/src/widgets/list_edge_fade.dart
+++ lib/src/widgets/list_edge_fade.dart
@@ -1,34 +1,56 @@
 import 'package:flutter/material.dart';
 
-class ListEdgeFade extends StatelessWidget {
-  const ListEdgeFade({
+class EdgeFadeOptions {
+  final bool enabled;
+  final double heightFactor;
+  final double halfWayPoint;
+
+  const EdgeFadeOptions({
+    required this.enabled,
+    required this.heightFactor,
+    required this.halfWayPoint,
+  });
+}
+
+class EdgeFade extends StatelessWidget {
+  const EdgeFade({
     super.key,
     required this.child,
-    this.top = true,
-    this.bottom = true,
-    this.fadeHeightFactor = 0.1,
+    this.topOptions = _defaultOptions,
+    this.bottomOptions = _defaultOptions,
   });
 
   final Widget child;
-  final bool top;
-  final bool bottom;
-  final double fadeHeightFactor;
+  final EdgeFadeOptions topOptions;
+  final EdgeFadeOptions bottomOptions;
+
+  static const EdgeFadeOptions _defaultOptions = EdgeFadeOptions(
+    enabled: true,
+    heightFactor: 0.1,
+    halfWayPoint: 0.5,
+  );
 
   @override
   Widget build(BuildContext context) {
     final List<Color> colors = [];
     final List<double> stops = [];
 
-    if (top) {
+    if (topOptions.enabled) {
       colors.add(Colors.transparent);
       stops.add(0.0);
+      colors.add(Color(0x80000000).withValues(alpha: topOptions.halfWayPoint));
+      stops.add(topOptions.heightFactor / 2);
       colors.add(Colors.black);
-      stops.add(fadeHeightFactor);
+      stops.add(topOptions.heightFactor);
     }
 
-    if (bottom) {
+    if (bottomOptions.enabled) {
       colors.add(Colors.black);
-      stops.add(1 - fadeHeightFactor);
+      stops.add(1 - bottomOptions.heightFactor);
+      colors.add(
+        Color(0x80000000).withValues(alpha: bottomOptions.halfWayPoint),
+      );
+      stops.add(1 - bottomOptions.heightFactor / 2);
       colors.add(Colors.transparent);
       stops.add(1.0);
     }
