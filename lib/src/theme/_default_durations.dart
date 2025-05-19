63b62fdd2656ffc3f64c72d32a94c47ca6baaf7f
Anton <anton@antons-webfabrik.eu>
2025-05-19 16:49:44 +0200

    feat: add durations to theme

--- lib/src/theme/_default_durations.dart
+++ lib/src/theme/_default_durations.dart
@@ -4,5 +4,15 @@ part of 'webfabrik_theme.dart';
 ///
 /// The default miscellaneous values for the [WebfabrikTheme].
 class _DefaultDurations {
+  const _DefaultDurations();
+
+  static const Duration tiny = Duration(milliseconds: 50);
+  static const Duration xTiny = Duration(milliseconds: 100);
+  static const Duration xxTiny = Duration(milliseconds: 150);
   static const Duration short = Duration(milliseconds: 200);
+  static const Duration xShort = Duration(milliseconds: 250);
+  static const Duration xxShort = Duration(milliseconds: 300);
+  static const Duration medium = Duration(milliseconds: 400);
+  static const Duration xMedium = Duration(milliseconds: 500);
+  static const Duration xxMedium = Duration(milliseconds: 600);
 }
--- lib/src/theme/webfabrik_durations_theme_data.dart
+++ lib/src/theme/webfabrik_durations_theme_data.dart
@@ -4,11 +4,57 @@ part of 'webfabrik_theme.dart';
 ///
 /// A collection of durations for the [WebfabrikTheme].
 class WebfabrikDurationsThemeData {
-  const WebfabrikDurationsThemeData({Duration? short}) : this._(short);
+  const WebfabrikDurationsThemeData({
+    Duration? tiny,
+    Duration? xTiny,
+    Duration? xxTiny,
+    Duration? short,
+    Duration? xShort,
+    Duration? xxShort,
+    Duration? medium,
+    Duration? xMedium,
+    Duration? xxMedium,
+  }) : this._(
+         tiny ?? _DefaultDurations.tiny,
+         xTiny ?? _DefaultDurations.xTiny,
+         xxTiny ?? _DefaultDurations.xxTiny,
+         short ?? _DefaultDurations.short,
+         xShort ?? _DefaultDurations.xShort,
+         xxShort ?? _DefaultDurations.xxShort,
+         medium ?? _DefaultDurations.medium,
+         xMedium ?? _DefaultDurations.xMedium,
+         xxMedium ?? _DefaultDurations.xxMedium,
+       );
 
-  const WebfabrikDurationsThemeData._(this._short);
+  const WebfabrikDurationsThemeData._(
+    this._tiny,
+    this._xTiny,
+    this._xxTiny,
+    this._short,
+    this._xShort,
+    this._xxShort,
+    this._medium,
+    this._xMedium,
+    this._xxMedium,
+  );
 
+  final Duration? _tiny;
+  final Duration? _xTiny;
+  final Duration? _xxTiny;
   final Duration? _short;
+  final Duration? _xShort;
+  final Duration? _xxShort;
+  final Duration? _medium;
+  final Duration? _xMedium;
+  final Duration? _xxMedium;
 
+  Duration get tiny => _tiny ?? _DefaultDurations.tiny;
+  Duration get xTiny => _xTiny ?? _DefaultDurations.xTiny;
+  Duration get xxTiny => _xxTiny ?? _DefaultDurations.xxTiny;
   Duration get short => _short ?? _DefaultDurations.short;
+  Duration get xShort => _xShort ?? _DefaultDurations.xShort;
+  Duration get xxShort => _xxShort ?? _DefaultDurations.xxShort;
+  Duration get medium => _medium ?? _DefaultDurations.medium;
+  Duration get xMedium => _xMedium ?? _DefaultDurations.xMedium;
+  Duration get xxMedium => _xxMedium ?? _DefaultDurations.xxMedium;
 }
