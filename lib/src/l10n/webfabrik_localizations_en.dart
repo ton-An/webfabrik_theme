448f203e62fcba4645a36f1e7ed0bdd8e2209b08
Anton <anton@antons-webfabrik.eu>
2025-09-09 19:15:33 +0200

    feat: add localization support with English translations and configuration

---
+++ lib/src/l10n/webfabrik_localizations_en.dart
@@ -0,0 +1,16 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'webfabrik_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class WebfabrikLocalizationsEn extends WebfabrikLocalizations {
+  WebfabrikLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get attributions => 'Attributions';
+
+  @override
+  String get ok => 'Ok';
+}
