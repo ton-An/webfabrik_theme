bd58598e263ce164be66be856fc6c75e219a1c8f
Anton <anton@antons-webfabrik.eu>
2026-06-13 18:27:28 +0200

    feat(theme): migrate typography to Rubik

--- lib/src/theme/_default_text_styles.dart
+++ lib/src/theme/_default_text_styles.dart
@@ -5,101 +5,111 @@ part of 'webfabrik_theme.dart';
 /// The default text styles for the [WebfabrikTheme].
 class _DefaultTextStyles {
   static const TextStyle largeTitle = TextStyle(
-    fontFamily: 'Inter',
-    package: 'webfabrik_theme',
+    fontFamily: 'Rubik',
     fontSize: 34,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w500,
     height: 41 / 34,
+    letterSpacing: 0.6,
+    wordSpacing: 1,
     color: CupertinoColors.label,
   );
 
   static const TextStyle title1 = TextStyle(
-    fontFamily: 'Inter',
-    package: 'webfabrik_theme',
+    fontFamily: 'Rubik',
     fontSize: 28,
-    fontVariations: [FontVariation('wght', 400)],
+    fontWeight: FontWeight.w500,
     height: 34 / 28,
+    letterSpacing: 0.6,
+    wordSpacing: 1,
     color: CupertinoColors.label,
   );
 
   static const TextStyle title2 = TextStyle(
-    fontFamily: 'Inter',
-    package: 'webfabrik_theme',
+    fontFamily: 'Rubik',
     fontSize: 22,
-    fontVariations: [FontVariation('wght', 400)],
+    fontWeight: FontWeight.w500,
     height: 28 / 22,
+    letterSpacing: 0.4,
     color: CupertinoColors.label,
   );
 
   static const TextStyle title3 = TextStyle(
-    fontFamily: 'Inter',
-    package: 'webfabrik_theme',
+    fontFamily: 'Rubik',
     fontSize: 20,
-    fontVariations: [FontVariation('wght', 400)],
+    fontWeight: FontWeight.w500,
     height: 25 / 20,
+    letterSpacing: 0.4,
+    wordSpacing: 1,
     color: CupertinoColors.label,
   );
 
   static const TextStyle headline = TextStyle(
-    fontFamily: 'Inter',
-    package: 'webfabrik_theme',
+    fontFamily: 'Rubik',
     fontSize: 17,
-    fontVariations: [FontVariation('wght', 600)],
+    fontWeight: FontWeight.w500,
     height: 22 / 17,
+    letterSpacing: -.4,
+    wordSpacing: .7,
     color: CupertinoColors.label,
   );
 
   static const TextStyle body = TextStyle(
-    fontFamily: 'Inter',
-    package: 'webfabrik_theme',
-    fontSize: 18,
-    fontVariations: [FontVariation('wght', 400)],
+    fontFamily: 'Rubik',
+    fontSize: 17,
+    fontWeight: FontWeight.w400,
     height: 22 / 17,
+    letterSpacing: -0.41,
+    wordSpacing: 1.2,
     color: CupertinoColors.label,
   );
 
   static const TextStyle callout = TextStyle(
-    fontFamily: 'Inter',
-    package: 'webfabrik_theme',
+    fontFamily: 'Rubik',
     fontSize: 16,
-    fontVariations: [FontVariation('wght', 400)],
+    fontWeight: FontWeight.w400,
     height: 21 / 16,
+    letterSpacing: -0.32,
+    wordSpacing: 1,
     color: CupertinoColors.label,
   );
 
   static const TextStyle subhead = TextStyle(
-    fontFamily: 'Inter',
-    package: 'webfabrik_theme',
+    fontFamily: 'Rubik',
     fontSize: 15,
-    fontVariations: [FontVariation('wght', 400)],
+    fontWeight: FontWeight.w400,
     height: 20 / 15,
+    letterSpacing: -0.24,
+    wordSpacing: 1,
     color: CupertinoColors.label,
   );
 
   static const TextStyle footnote = TextStyle(
-    fontFamily: 'Inter',
-    package: 'webfabrik_theme',
+    fontFamily: 'Rubik',
     fontSize: 13.0,
-    fontVariations: [FontVariation('wght', 400)],
+    fontWeight: FontWeight.w400,
     height: 18 / 13,
+    letterSpacing: -0.08,
+    wordSpacing: .7,
     color: CupertinoColors.label,
   );
 
   static const TextStyle caption1 = TextStyle(
-    fontFamily: 'Inter',
-    package: 'webfabrik_theme',
+    fontFamily: 'Rubik',
     fontSize: 12,
-    fontVariations: [FontVariation('wght', 400)],
+    fontWeight: FontWeight.w400,
     height: 16 / 12,
+    letterSpacing: 0,
+    wordSpacing: .7,
     color: CupertinoColors.label,
   );
 
   static const TextStyle caption2 = TextStyle(
-    fontFamily: 'Inter',
-    package: 'webfabrik_theme',
+    fontFamily: 'Rubik',
     fontSize: 11,
-    fontVariations: [FontVariation('wght', 400)],
+    fontWeight: FontWeight.w400,
     height: 13 / 11,
+    letterSpacing: 0.07,
+    wordSpacing: .7,
     color: CupertinoColors.label,
   );
 }
